3f3e21c150775882056c79341649489b602fa515
Dilshod170520 <dilshodqulmirzayev21@gmail.com>
2025-12-03 11:42:15 +0500

    toobbar visibility hidden for bottom

--- Sources/SwiftUICustomRouter/Components/NavigationStackIfNeeded.swift
+++ Sources/SwiftUICustomRouter/Components/NavigationStackIfNeeded.swift
@@ -19,6 +19,7 @@ struct NavigationStackIfNeeded<Content: View>: View {
                 content
                     .navigationDestination(for: AnyDestination.self) { value in
                         value.destination
+                            .toolbarVisibility(.hidden, for: .bottomBar)
                     }
             }
         } else {
